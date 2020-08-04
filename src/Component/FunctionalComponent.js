d66443659ee86bdf92645d95e586f21868949778
Prashant Singh <prashant.55555@yahoo.co.in>
2020-08-04 23:26:13 +0530

    Component Type

---
+++ src/Component/FunctionalComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function functioCompo(){
+    return(
+        <>
+        <b style={{color:'lightsalmon'}}>Hi,Welcome to Functional Component.</b>
+        </>
+    )
+}
+export default functioCompo;
