85f9cb5c384a0be557ee1c430ea1628017cecc30
Prashant Singh <prashant.55555@yahoo.co.in>
2020-08-05 20:19:58 +0530

    Use of State and Props and parent to child props use

--- src/Component/ClassComponent.js
+++ src/Component/ClassComponent.js
@@ -1,17 +1,22 @@
 import React,{Component} from 'react';
+import { Tooltip } from '@material-ui/core';
 //import {Grid} from '@material-ui/core';
 //import Footer from './FooterComponent';
 class ClassComp extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            name:'Prashant Singh',
+            language:'React.js'
+        }
+        this.props.nickName('Motto');
+    }
+   
     render(){
         return(
             <>
-            <b style={{color:'lightgreen'}}>Hi,Welcome to Class Component.</b>
-            {/* <Grid container>
-                <Grid item>
-                <Footer></Footer>
-                </Grid>
-            </Grid> */}
-           
+            <b style={{color:'lightgreen'}}>Hi,<Tooltip title="Use of Props"><b style={{color:'white'}}>{this.props.name}</b></Tooltip>Welcome to Class Component.</b><br/>
+           <b style={{color:'lightgreen'}}>Hi,<Tooltip title="Use of state"><b style={{color:'black'}}>{this.state.name}</b></Tooltip>Welcome to <b style={{color:'black'}}>{this.state.language}</b> Class Component</b>
             </>
         )
     }
--- src/Component/FunctionalComponent.js
+++ src/Component/FunctionalComponent.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Tooltip } from '@material-ui/core';
 
-function functioCompo(){
+function functioCompo(props){
+   // props.nickName('Guddan');
     return(
         <>
-        <b style={{color:'lightsalmon'}}>Hi,Welcome to Functional Component.</b>
+        <b style={{color:'lightsalmon'}}>Hi,<Tooltip title="use of props"><b style={{color:'white'}}>{props.name}</b></Tooltip>Welcome to Functional Component.</b>
         </>
     )
 }
